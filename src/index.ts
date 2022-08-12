c8971f33b237486035a8dc0438eb1c98fea2c917
MJ <110538262+MJ-Nanni@users.noreply.github.com>
2022-08-12 20:39:33 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import "./styles.css";
+const p = document.querySelector("p");
+const hayUnaPila : boolean = true
+
+if (hayUnaPila === true){
+  p.textContent = ("Quitar pila");
+} else if ("!hayUnaPila"){
+p.textContent = ("Llenar agujero");}
+
+
+
+
